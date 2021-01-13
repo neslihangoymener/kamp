03168aff0217a4fbc5ebc6de9f98e11e81e2afe9
neslihangoymener <77414839+neslihangoymener@users.noreply.github.com>
2021-01-14 00:26:17 +0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,12 @@
+
+class Banka:
+    def krediBasvur(self):
+        print("Kredi Başvurusu Yapıldı")
+
+    def krediHesaplama(self):
+        print("Krediler Hesaplandı")
+
+banka = Banka()
+banka.krediBasvur
+
+# self = this
