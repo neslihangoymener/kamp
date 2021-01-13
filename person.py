22fb46d7af4b2a4fef637a5850a3ebc3e0aa3ad0
neslihangoymener <77414839+neslihangoymener@users.noreply.github.com>
2021-01-14 00:26:54 +0300

    Add files via upload

---
+++ person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name,lastname) :
+        #super().__init__()
+        self.name = name
+        self.lastname = lastname
+
+
+
+
+musteri1 = Person("Neslihan","KUŞ")
+musteri2 = Person("Murat","GÖYMENER")
+musteri3 = Person("Ayşe","Fatma")
+print (musteri1.name)
